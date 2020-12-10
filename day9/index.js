fd3eb5a8d71bcd076998363fcb94726572fac638
Ben Anderson <andersjbe@gmail.com>
2020-12-10 09:52:56 -0700

    Day 9 part 2

--- day9/index.js
+++ day9/index.js
@@ -14,6 +14,26 @@ function hasSum(nums, sum) {
     return false;
 }
 
+function findContiguousNums(nums, sum) {
+    for (let i=0; i<nums.length; i++) {
+        for (let j=nums.length-1; j>i+1; j--) {
+            let range = nums.slice(i, j+1)
+            let constiguousSum = range.reduce((acc, num) => acc + num, 0);
+            if (sum === constiguousSum) {
+                const min = range.reduce((num, acc) => {
+                    if (num < acc) acc = num;
+                    return acc;
+                }, Infinity)
+                const max = range.reduce((num, acc) => {
+                    if (num > acc) acc = num;
+                    return acc;
+                }, -Infinity)
+                return min + max
+            }
+        }
+    }
+}
+
 (async function main() {
     let lines = [];
     await fs.readFile('day9/data', 'utf8', (err, data) => {
@@ -30,6 +50,7 @@ function hasSum(nums, sum) {
             let range = lines.slice(i, sum);
             if (!hasSum(range, lines[sum])) {
                 console.log(lines[sum]);
+                console.log(findContiguousNums(lines, lines[sum]));
                 return;
             }
             i++;
